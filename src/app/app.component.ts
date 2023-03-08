098e29d851ddced5db3d4983dbf818a939d20735
Jose daniel <jose.dejesus@it-and-apps.mx>
2023-03-08 10:52:23 -0600

    update name Reedlatino to Orion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { PrimeNGConfig } from 'primeng/api';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-    title = 'FEReedAwards';
+    title = 'Orion';
 
     constructor(private primengConfig: PrimeNGConfig) {}
 
